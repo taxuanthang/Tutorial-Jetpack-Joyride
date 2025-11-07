0f8209f4975f2511c27c7701e1a213b43699e934
taxuanthang <thangtaxuan65@gmail.com>
2025-11-07 23:46:30 +0700

    UI_Part 2: Create Setting Mennu and UI Animations

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,8 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
+    public Animator dialog;
+
+
     public void StartGame()
     {
         SceneManager.LoadScene("RocketMouse");
     }
+
+    public void OpenSettings()
+    {
+        startButton.SetBool("isHidden", true);
+        settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings()
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
+    }
+
+
 }
